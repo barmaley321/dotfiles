73395c1dd6de6d42e6567f178f6b2cf6fc51d9c0
barmaley321 <lkjhlkjh.kjhgjhgkj@yandex.ru>
2017-09-30 00:33:04 +0600

    check rc.lua theme path

--- dotfiles/rc.lua
+++ dotfiles/rc.lua
@@ -39,7 +39,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 --beautiful.init("/usr/share/awesome/themes/default/theme.lua")
-beautiful.init("/home/anatol/.awesome/theme.lua")
+beautiful.init("~/.awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
