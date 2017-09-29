4bfd60f16e797044c8a083c0b87926a72f77c659
barmaley321 <lkjhlkjh.kjhgjhgkj@yandex.ru>
2017-09-30 02:19:08 +0600

    add tmuxinator daily.yml config

--- install_dot.sh
+++ install_dot.sh
@@ -6,7 +6,13 @@ dir=~/.dotfiles/dotfiles
 olddir=~/.dotfiles/dotfiles_old
 files_user="zshrc zshrc.local vimrc xinitrc Xdefaults"
 files_awesome="rc.lua theme.lua metall_wp.png"
+dir_awesome="awesome"
 files_mpv="mpv.conf"
+dir_mpv="config/mpv"
+files_tmuxinator="daily.yml"
+dir_tmuxinator="tmuxinator"
+
+
 
 ### create dotfiles_old in .dotfiles dir:
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -28,28 +34,39 @@ done
 
 # move any existing awesome config files to dotfiles_old directory, then create symlinks 
 for file in $files_awesome; do
-    if [ ! -L ~/.awesome/$file ]; then
-        echo "Moving any existing configs from ~/.awesome/ to $olddir"
-        mv ~/.awesome/$file $olddir
+    if [ ! -L ~/.$dir_awesome/$file ]; then
+        echo "Moving any existing configs from ~/.$dir_awesome/ to $olddir"
+        mv ~/.$dir_awesome/$file $olddir
         echo "Creating symlink to $file in home directory."
-        ln -s $dir/$file ~/.awesome/$file
+        ln -s $dir/$file ~/.$dir_awesome/$file
     else
         echo "symlink exists"
-        ls -al ~/$file
+        ls -al ~/.$dir_awesome/$file
     fi
 done
 
 # move any existing mpv config files to dotfiles_old directory, then create symlinks 
 for file in $files_mpv; do
-    if [ ! -L ~/.config/mpv/$file ]; then
-        echo "Moving any existing configs from ~/.config/mpv/ to $olddir"
-        mv ~/.config/mpv/$file $olddir
+    if [ ! -L ~/.$dir_mpv/$file ]; then
+        echo "Moving any existing configs from ~/.$dir_mpv/ to $olddir"
+        mv ~/.$dir_mpv/$file $olddir
         echo "Creating symlink to $file in home directory."
-        ln -s $dir/$file ~/.config/mpv/$file
+        ln -s $dir/$file ~/.$dir_mpv/$file
     else
         echo "symlink exists"
-        ls -al ~/$file
+        ls -al ~/.$dir_mpv/$file
     fi
 done
 
-
+#tmuxinator:
+for file in $files_tmuxinator; do
+    if [ ! -L ~/.$dir_tmuxinator/$file ]; then
+        echo "Moving any existing configs from $dir_tmuxinator to $olddir"
+        mv ~/.$dir_tmuxinator/$file $olddir
+        echo "Creating symlink to $file in home directory."
+        ln -s $dir/$file ~/.$dir_tmuxinator/$file
+    else
+        echo "symlink exists"
+        ls -al ~/.$dir_tmuxinator/$file
+    fi
+done
